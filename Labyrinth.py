4b34a9e53176a361db90df747a4c315af43ac20a
rLutz44 <robertlutz35@yahoo.de>
2021-10-03 14:32:14 +0200

    splitting into another file for more clarity

--- Labyrinth.py
+++ Labyrinth.py
@@ -1,93 +1,78 @@
 import numpy as np
+
 import random
 
-def labyrinthBuilder(labyrinthNumberOfLevels, labyrinthNumberOfRows, labyrinthNumberOfColumns):
- 
-    if(labyrinthNumberOfLevels == 0 and labyrinthNumberOfRows == 0 and labyrinthNumberOfColumns == 0):   
-        return "0 0 0"
-    if(labyrinthNumberOfLevels == 0 or labyrinthNumberOfRows == 0 or labyrinthNumberOfColumns == 0):
-        return "Only use 0 for all 3 inputs or none of them, pls try again\n"
-    if(labyrinthNumberOfRows + labyrinthNumberOfColumns < 3):
-        return "The sum of labyrinth rows and columns should not be less than 3, pls try again"
- 
-    labyrinthLevelList = []
-    while(len(labyrinthLevelList) < labyrinthNumberOfLevels):
-        labyrinthLevel = labyrinthLevelDesigner(labyrinthNumberOfRows, labyrinthNumberOfColumns)
-        labyrinthLevelList.append(labyrinthLevel)
+from labyrinth_builder import labyrinth_builder
+
+
+#--------Part of the Code to get out of the labyrinth---------
+
+def canIGetOutOfLabyrinth(labyrinth_level_list : list):
+    """
+    | Searching shortest path from start to exit with A* algorithm
     
-    labyrinthLevelList = labyrinthSetStartAndExit(labyrinthLevelList, labyrinthNumberOfRows, labyrinthNumberOfColumns)
+    """
     
-    print("\n")
-    print(labyrinthLevelList[0])
-    print("\n")
-    print(labyrinthLevelList[1])
-
-
-
-def labyrinthLevelDesigner(labyrinthNumberOfRows, labyrinthNumberOfColumns):
     
-    labyrinthWall = "#"
-    labyrinthAir = "."
-    labyrinthLevel = np.chararray((labyrinthNumberOfRows, labyrinthNumberOfColumns), unicode = True)    
+class Node:
+   """
+    | Nodes are the squares in the labyrinth there is at least 1 path to go that is not a dead end
+   """
+   def __init__(self, labyrinth_row: int, labyrinth_column: int, labyrinth_dead_end: bool = False):
+        self.labyrinth_row = labyrinth_row
+        self.labyrinth_column = labyrinth_column
+        self.labyrinth_neighbors = []
+        self.labyrinth_dead_end = labyrinth_dead_end        # Dead_end when there is only 1 path to go or when the path has already been tested and resulted in a dead end
+        
+   def get_dead_end(self):
+       return self.labyrinth_dead_end
 
-    labyrinthComponentList = [labyrinthWall, labyrinthAir]
-    
-    for labyrinthRow in range(labyrinthNumberOfRows):
-        for labyrinthColumn in range (labyrinthNumberOfColumns):
-            labyrinthComponent = random.choice(labyrinthComponentList)
-            labyrinthLevel[labyrinthRow][labyrinthColumn] = labyrinthComponent 
-            
-    return labyrinthLevel
 
 
-def labyrinthSetStartAndExit(labyrinthLevelList, labyrinthNumberOfRows, labyrinthNumberOfColumns):
-    
-    labyrinthStart = "S"
-    labyrinthExit = "E"    
-    
-    labyrinthLevelFirst = 0
-    labyrinthRowFirstIdx = 0
-    labyrinthColumnFirstIdx = 0
-    
-    labyrinthLevelLast = len(labyrinthLevelList)-1
-    labyrinthRowLastIdx = labyrinthNumberOfRows-1
-    labyrinthColumnLastIdx = labyrinthNumberOfColumns-1
 
-    labyrinthLevelList[labyrinthLevelFirst][labyrinthRowFirstIdx][labyrinthColumnFirstIdx] = labyrinthStart
-    labyrinthLevelList[labyrinthLevelLast][labyrinthRowLastIdx][labyrinthColumnLastIdx] = labyrinthExit
 
-    return labyrinthLevelList
 
-#def canIGetOut():
+
+
+
+
+
+
+
 
 
 #------------------- Main ---------------------#
 
-print("Welcome to the Labyrinth Maker where you can create your own Labyrinth\nPls enter how many levels, rows and columns you want for your Labyrinth\nAllowed Numbers are integers from 0 to 30\n\n")
-
-while (True):
-    labyrinthNumberOfLevels = input("Pls enter the number of Levels for this Labyrinth.  \n")
-    labyrinthNumberOfLevels = int(labyrinthNumberOfLevels)
-    if (labyrinthNumberOfLevels >= 0 and labyrinthNumberOfLevels < 31):
-        break
-    print("Invalid Input, use integer from 0 to 30\n")
-
-while (True):
-    labyrinthNumberOfRows = input("How many rows should it have?\n")
-    labyrinthNumberOfRows = int(labyrinthNumberOfRows)
-    if (labyrinthNumberOfLevels >= 0 and labyrinthNumberOfLevels < 31):
-        break
-    print("Invalid Input, use integer from 0 to 30\n")
-
-while (True):
-    labyrinthNumberOfColumns = input("How many columns do you want to have?\n")
-    labyrinthNumberOfColumns = int(labyrinthNumberOfColumns)
-    if (labyrinthNumberOfLevels >= 0 and labyrinthNumberOfLevels < 31):
-        break
-    print("Invalid Input, use integer from 0 to 30\n")
+if (__name__ == "__main__"):
     
-#print(labyrinthNumberOfLevels)
-#print(labyrinthNumberOfRows)
-#print(labyrinthNumberOfColumns)
-
-labyrinthBuilder(labyrinthNumberOfLevels, labyrinthNumberOfRows, labyrinthNumberOfColumns)
+    print("Welcome to the Labyrinth Maker where you can create your own Labyrinth\nPls enter how many levels, rows and columns you want for your Labyrinth\nAllowed Numbers are integers from 0 to 30\n\n")
+    
+    while (True):
+        labyrinth_number_of_levels = input("Pls enter the number of Levels for this Labyrinth.  \n")
+        labyrinth_number_of_levels = int(labyrinth_number_of_levels)
+        if (labyrinth_number_of_levels >= 0 and labyrinth_number_of_levels < 31):
+            break
+        print("Invalid Input, use integer from 0 to 30\n")
+    
+    while (True):
+        labyrinth_number_of_rows = input("How many rows should it have?\n")
+        labyrinth_number_of_rows = int(labyrinth_number_of_rows)
+        if (labyrinth_number_of_levels >= 0 and labyrinth_number_of_levels < 31):
+            break
+        print("Invalid Input, use integer from 0 to 30\n")
+    
+    while (True):
+        labyrinth_number_of_columns = input("How many columns do you want to have?\n")
+        labyrinth_number_of_columns = int(labyrinth_number_of_columns)
+        if (labyrinth_number_of_levels >= 0 and labyrinth_number_of_levels < 31):
+            break
+        print("Invalid Input, use integer from 0 to 30\n")
+        
+    
+    labyrinth_level_list = labyrinth_builder(labyrinth_number_of_levels, labyrinth_number_of_rows, labyrinth_number_of_columns)
+    # print("\n")
+    # print(labyrinth_level_list[0])
+    # print("\n")
+    # print(labyrinth_level_list[1])
+    
+   # canIGetOutOfLabyrinth(labyrinth_level_list)
---
+++ labyrinth_builder.py
@@ -0,0 +1,77 @@
+import numpy as np
+
+import random
+
+
+
+def labyrinth_builder(
+        labyrinth_number_of_levels: int, 
+        labyrinth_number_of_rows: int, 
+        labyrinth_number_of_columns: int) -> list:
+    """
+    | Assemble all Levels into a single labyrinth and prints the whole labyrinth
+    
+    """
+    if(labyrinth_number_of_levels == 0 and labyrinth_number_of_rows == 0 and labyrinth_number_of_columns == 0):   
+        return "0 0 0"
+    
+    if(labyrinth_number_of_levels == 0 or labyrinth_number_of_rows == 0 or labyrinth_number_of_columns == 0):
+        return "Only use 0 for all 3 inputs or none of them, pls try again\n"
+    
+    if(labyrinth_number_of_rows + labyrinth_number_of_columns < 3):
+        return "The sum of labyrinth rows and columns should not be less than 3, pls try again"
+ 
+    labyrinth_level_list: list = []
+    while(len(labyrinth_level_list) < labyrinth_number_of_levels):
+        labyrinth_level: str = labyrinth_level_designer(labyrinth_number_of_rows, labyrinth_number_of_columns)
+        labyrinth_level_list.append(labyrinth_level)
+    
+    labyrinth_level_list = labyrinth_set_start_and_exit(labyrinth_level_list, labyrinth_number_of_rows, labyrinth_number_of_columns)
+    
+    print("\n")
+    print(labyrinth_level_list[0])
+    print("\n")
+    print(labyrinth_level_list[1])
+
+    return labyrinth_level_list
+
+def labyrinth_level_designer(labyrinth_number_of_rows: int, labyrinth_number_of_columns: int) -> str:
+    """
+    | Designing and Creating one level of the labyrinth
+    
+    """
+    LABYRINTH_WALL: str             = "#"
+    LABYRINTH_AIR: str              = "."
+    LABYRINTH_COMPONENT_LIST: str   = [LABYRINTH_WALL, LABYRINTH_AIR]
+    labyrinth_level: str            = np.chararray((labyrinth_number_of_rows, labyrinth_number_of_columns), unicode = True)    
+    
+    for labyrinth_row in range(labyrinth_number_of_rows):
+        for labyrinth_column in range (labyrinth_number_of_columns):
+            labyrinth_component: str                            = random.choice(LABYRINTH_COMPONENT_LIST)
+            labyrinth_level [labyrinth_row] [labyrinth_column]  = labyrinth_component 
+            
+    return labyrinth_level
+
+
+def labyrinth_set_start_and_exit(labyrinth_level_list: str, labyrinth_number_of_rows: int, labyrinth_number_of_columns: int) -> list:
+    """
+    | Sets the starting point and the exit for the labyrinth
+    
+    """
+    LABYRINTH_START: str                = "S"
+    LABYRINTH_EXIT: str                 = "E"    
+    
+    LABYRINTH_FIRST_LEVEL: int          = 0
+    LABYRINTH_FIRST_INDEX_ROW: int      = 0
+    LABYRINTH_FIRST_INDEX_COLUMN: int   = 0
+    
+    labyrinth_last_level: int           = len(labyrinth_level_list)-1
+    labyrinth_last_index_row: int       = labyrinth_number_of_rows-1
+    labyrinth_last_index_column: int    = labyrinth_number_of_columns-1
+
+    labyrinth_level_list [LABYRINTH_FIRST_LEVEL] [LABYRINTH_FIRST_INDEX_ROW] [LABYRINTH_FIRST_INDEX_COLUMN] = LABYRINTH_START
+    labyrinth_level_list [labyrinth_last_level] [labyrinth_last_index_row] [labyrinth_last_index_column]    = LABYRINTH_EXIT
+
+    return labyrinth_level_list
+
+
