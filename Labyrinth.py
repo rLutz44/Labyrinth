f12243b092ad8a612ea46c16a894b2d1da78ce58
rLutz44 <robertlutz35@yahoo.de>
2021-10-04 11:45:40 +0200

    labyrinth_node_grid_creator
    Implementation of the labyrinth_node_grid_creator which is needed for the A* algorithm later.

--- Labyrinth.py
+++ Labyrinth.py
@@ -6,7 +6,36 @@ from labyrinth_builder import labyrinth_builder
 
 
 #--------Part of the Code to get out of the labyrinth---------
+class Node:
+    """
+    | Nodes are any places in the labyrinth you can visit with atleast 3 branches
+    
+    """
+    def __init__(
+           self, 
+           labyrinth_level: int,
+           labyrinth_row: int, 
+           labyrinth_column: int,
+           labyrinth_compound: str):
+       
+        self.labyrinth_level: int = labyrinth_level #z
+        self.labyrinth_row: int = labyrinth_row #y
+        self.labyrinth_column: int = labyrinth_column #x
+        self.labyrinth_compound: str = labyrinth_compound
+        self.labyrinth_neighbors: list = []
 
+    
+    def update_neighbors(self):
+        self.neighbors = []
+    
+    def is_wall(self):
+        return self.labyrinth_compound == "X"
+    
+    def is_air(self):
+        return  self.labyrinth_compound == "."
+    
+    
+    
 def canIGetOutOfLabyrinth(
         labyrinth_level_list : list,
         labyrinth_number_of_levels: int, 
@@ -16,8 +45,11 @@ def canIGetOutOfLabyrinth(
     | Searching shortest path from start to exit with A* algorithm if it's possible
     
     """
-    #create_labyrinth_node_grid(labyrinth_level_list)
-    
+    labyrinth_node_grid: np = create_labyrinth_node_grid(labyrinth_level_list,
+                                                         labyrinth_number_of_levels, 
+                                                         labyrinth_number_of_rows, 
+                                                         labyrinth_number_of_columns)
+                                
     # LABYRINTH_START: int = 0
     # h_distance_from_node_to_exit = ((labyrinth_number_of_levels-1) 
     #                                                    + (labyrinth_number_of_rows-1) 
@@ -29,48 +61,36 @@ def canIGetOutOfLabyrinth(
     #                     + g_distance_start_to_node) #the lower the score the higher the priority
     #bei start node ist f score = 0
     
-class Node:
+
+    
+    
+
+def create_labyrinth_node_grid(labyrinth_level_list : list,
+                               labyrinth_number_of_levels: int, 
+                               labyrinth_number_of_rows: int, 
+                               labyrinth_number_of_columns: int) -> np:
     """
-    | Nodes are any places in the labyrinth you can visit with atleast 3 branches
+    | Creating numpy array that contains instances from node representing the labyrinth
     
     """
-    def __init__(
-           self, 
-           labyrinth_level: int,
-           labyrinth_row: int, 
-           labyrinth_column: int, 
-           labyrinth_dead_end: bool = False):
-       
-        self.labyrinth_level = labyrinth_level
-        self.labyrinth_row = labyrinth_row
-        self.labyrinth_column = labyrinth_column
-        self.labyrinth_neighbors = []
-        self.labyrinth_compound = "."
-        self.labyrinth_dead_end = labyrinth_dead_end        # Dead_end when there is only 1 path to go or when the path has already been tested and resulted in a dead end
-        
-    def get_pos(self):
-        return self.labyrinth_row, self.labyrinth_column
-       
-    def is_wall(self):
-        return self.labyrinth_compound == "X"
     
-    def is_air(self):
-        return  self.labyrinth_air == "."
+    # start = Node(0, 0, 0, "ST")
+    # second = Node(0,0,1, "Hi")
+    # setattr(start, 'labyrinth_compound', "SG")
+
+    #numpy array with objects and level are missing
+    labyrinth_node_grid = np.ndarray ((labyrinth_number_of_rows, labyrinth_number_of_columns), dtype=np.object )
+    for row in range(labyrinth_number_of_rows):
+        for column in range(labyrinth_number_of_columns):
+            labyrinth_node_grid[row][column] = Node(0, row, column, labyrinth_level_list[0][row][column])
+            #print(labyrinth_node_grid[row][column].labyrinth_compound)
+        #print()
     
-    def get_dead_end(self):
-         return self.labyrinth_dead_end
+    return labyrinth_node_grid
 
 
-def create_labyrinth_node_grid(labyrinth_level_list : list):
+def distance_node_to_exit():
     
-    
-
-
-
-
-
-
-
 
 
 
